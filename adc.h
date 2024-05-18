3d13833283701f95c4ceea35a3db4ea96005abeb
JoseWRPereira <josewrpereira@tutanota.com>
2024-05-18 16:05:50 -0300

    remoção de arquivos não necessários para o modelo

--- adc.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_H
-#define ADC_H
-
-void adc_init();
-void adc_start( void );
-int adc_ch0( void );
-int adc_ch1( void );
-
-#endif
--- timer0.h
+++
@@ -1,6 +0,0 @@
-#ifndef TIMER0_H
-#define TIMER0_H
-
-void timer0_init( void );
-
-#endif
